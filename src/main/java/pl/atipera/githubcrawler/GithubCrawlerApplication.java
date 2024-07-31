ad1a2344e254037c59e43f9945803a7fd44d62f3
bgnatowski <bgnatowski00@gmail.com>
2024-07-31 21:06:01 +0200

    init commit

---
+++ src/main/java/pl/atipera/githubcrawler/GithubCrawlerApplication.java
@@ -0,0 +1,12 @@
+package pl.atipera.githubcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/atipera/githubcrawler/GithubCrawlerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package pl.atipera.githubcrawler
+
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class LoadContextTest extends Specification {
+    def "when context is loaded then all expected beans are created"() {
+        expect: "the WebController is created"
+        1 == 1
+    }
+}
